4b8300b601ead8aa3e7c93b60f3632f250422e1a
Chih-Wei Huang <90484316+jerryhuangcw@users.noreply.github.com>
2022-03-22 05:29:58 -0700

    Add files via upload

---
+++ SC101_Assignment3/stanCodoshop.py
@@ -0,0 +1,159 @@
+"""
+File: stanCodoshop.py
+Name: Jerry Huang
+----------------------------------------------
+SC101_Assignment3
+Adapted from Nick Parlante's
+Ghost assignment by Jerry Liao.
+-----------------------------------------------
+"""
+
+import os
+import sys
+import math
+from simpleimage import SimpleImage
+
+
+def get_pixel_dist(pixel, red, green, blue):
+    """
+    Returns the color distance between pixel and mean RGB value
+
+    Input:
+        pixel (Pixel): pixel with RGB values to be compared
+        red (int): average red value across all images
+        green (int): average green value across all images
+        blue (int): average blue value across all images
+
+    Returns:
+        dist (int): color distance between red, green, and blue pixel values
+
+    """
+    color_distance = math.sqrt((red - pixel.red)**2 + (green - pixel.green)**2 + (blue - pixel.blue)**2)
+    return color_distance
+
+
+def get_average(pixels):
+    """
+    Given a list of pixels, finds the average red, blue, and green values
+
+    Input:
+        pixels (List[Pixel]): list of pixels to be averaged
+    Returns:
+        rgb (List[int]): list of average red, green, blue values across pixels respectively
+
+    Assumes you are returning in the order: [red, green, blue]
+
+    """
+    rgb = [0, 0, 0]
+    count = 0
+    for i in range(len(pixels)):
+        rgb[0] += pixels[i].red
+        rgb[1] += pixels[i].green
+        rgb[2] += pixels[i].blue
+        count += 1
+    rgb[0] //= count  # red avg
+    rgb[1] //= count  # green avg
+    rgb[2] //= count  # blue avg
+    return rgb
+
+
+def get_best_pixel(pixels):
+    """
+    Given a list of pixels, returns the pixel with the smallest
+    distance from the average red, green, and blue values across all pixels.
+
+    Input:
+        pixels (List[Pixel]): list of pixels to be averaged and compared
+    Returns:
+        best (Pixel): pixel closest to RGB averages
+
+    """
+    rgb = get_average(pixels)
+    best_dist = float('inf')  # shortest distance
+    best_pixel = 0
+    for i in range(len(pixels)):
+        dist = get_pixel_dist(pixels[i], rgb[0], rgb[1], rgb[2])
+        if dist < best_dist:
+            best_pixel = pixels[i]
+            best_dist = dist  # update
+    return best_pixel
+
+
+def solve(images):
+    """
+    Given a list of image objects, compute and display a Ghost solution image
+    based on these images. There will be at least 3 images and they will all
+    be the same size.
+
+    Input:
+        images (List[SimpleImage]): list of images to be processed
+    """
+    width = images[0].width
+    height = images[0].height
+    result = SimpleImage.blank(width, height)
+    # YOUR CODE STARTS HERE #
+    for x in range(result.width):
+        for y in range(result.height):
+            img_pixel = []
+            for i in images:
+                pixel = i.get_pixel(x, y)  # same pixels in different images
+                img_pixel.append(pixel)
+            best_pixel = get_best_pixel(img_pixel)
+            result_pixel = result.get_pixel(x, y)
+            result_pixel.red = best_pixel.red
+            result_pixel.green = best_pixel.green
+            result_pixel.blue = best_pixel.blue
+    # YOUR CODE ENDS HERE #
+    print("Displaying image!")
+    result.show()
+
+
+def jpgs_in_dir(dir):
+    """
+    (provided, DO NOT MODIFY)
+    Given the name of a directory, returns a list of the .jpg filenames
+    within it.
+
+    Input:
+        dir (string): name of directory
+    Returns:
+        filenames(List[string]): names of jpg files in directory
+    """
+    filenames = []
+    for filename in os.listdir(dir):
+        if filename.endswith('.jpg'):
+            filenames.append(os.path.join(dir, filename))
+    return filenames
+
+
+def load_images(dir):
+    """
+    (provided, DO NOT MODIFY)
+    Given a directory name, reads all the .jpg files within it into memory and
+    returns them in a list. Prints the filenames out as it goes.
+
+    Input:
+        dir (string): name of directory
+    Returns:
+        images (List[SimpleImages]): list of images in directory
+    """
+    images = []
+    jpgs = jpgs_in_dir(dir)
+    for filename in jpgs:
+        print("Loading", filename)
+        image = SimpleImage(filename)
+        images.append(image)
+    return images
+
+
+def main():
+    # (provided, DO NOT MODIFY)
+    args = sys.argv[1:]
+    # We just take 1 argument, the folder containing all the images.
+    # The load_images() capability is provided above.
+    images = load_images(args[0])
+    solve(images)
+
+
+if __name__ == '__main__':
+    main()
